99b49c4680e4d8ca6d985ef782a22fb95d9645a5
Dhruv Kapur <dhruvkapur4@gmail.com>
2021-11-22 20:49:41 +0530

    Fixed script paths in run.sh

--- run.sh
+++ run.sh
@@ -4,24 +4,24 @@ mkdir -p outputs
 cd scripts
 
 echo "Running name_scanner.py"
-python3 scripts/name_scanner.py
+python3 name_scanner.py
 echo "Done"
 echo
 
 echo "Running pl_scraper.py"
-python3 scripts/pl_scraper.py
+python3 pl_scraper.py
 echo "Done"
 echo
 
 # Call George's scripts here
 
 echo "Running clean_and_stats.py"
-python3 scripts/clean_and_stats.py
+python3 clean_and_stats.py
 echo "Done"
 echo
 
 echo "Running merge.py"
-python3 scripts/merge.py
+python3 merge.py
 echo "Done"
 
 cd ..
